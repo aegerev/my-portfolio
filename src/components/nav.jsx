e42790fa3d52e0d13db7d187a2665f250a682798
Alexander Egerev <alexandersegerev@gmail.com>
2024-02-24 06:56:41 -0500

    on-computer angel

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,11 +1,42 @@
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Nav() {
     return (
-        <>
-        <h1> NAV </h1>
-        <p> links go here </p>
-        </>
+               <div className='topnav'>
+            <div className='topnav-left'>
+                <NavLink
+                    activeclassname="navbar__link--active"
+                    className="navbar__link"
+                    to="/about"
+                >
+                    ABOUT ME
+                </NavLink>
+            </div>
+            <div className='topnav-right'>
+                <NavLink
+                    activeclassname="navbar__link--active"
+                    className="navbar__link"
+                    to="/portfolio"
+                >
+                    MY PORTFOLIO
+                </NavLink>
+                <NavLink
+                    activeclassname="navbar__link--active"
+                    className="navbar__link"
+                    to="/contact"
+                >
+                    CONTACT ME
+                </NavLink>
+                <NavLink
+                    activeclassname="navbar__link--active"
+                    className="navbar__link"
+                    to="/CurriculumVitae"
+                >
+                    MY RESUME
+                </NavLink>
+            </div>
+        </div>
     )
 }
 
