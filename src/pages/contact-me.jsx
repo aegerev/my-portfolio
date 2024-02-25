0e3a976c85fab1b9ba2cdb60d08cf6d42b767a7e
Alexander Egerev <alexandersegerev@gmail.com>
2024-02-25 13:15:22 -0500

    Added contact-me section

--- src/pages/contact-me.jsx
+++ src/pages/contact-me.jsx
@@ -4,8 +4,11 @@ function contactMe() {
 
     return (
         <>
-        <h1> Contact Me</h1>
-        <p> Something about me</p>
+        <h1> Contact Me </h1>
+
+        <p> Email: alexandersegerev@gmail.com</p>
+        <p> Phone: +1(609)475-2220</p>
+        <p>Github: https://github.com/upennbootcamp23</p>
         </>
     )
 };
