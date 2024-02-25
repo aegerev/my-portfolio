496ba361110190b5d61253afd71c2a0b0f3f5e6e
Alexander Egerev <alexandersegerev@gmail.com>
2024-02-25 11:48:03 -0500

    Updated portfolio

--- src/pages/my-portfolio.jsx
+++ src/pages/my-portfolio.jsx
@@ -1,12 +1,144 @@
 
-
-
 function myPortfolio() {
 
     return (
         <>
-        <h1> My Work </h1>
-        <p> Github and Dep;loy</p>
+   {/* START OF TABLE HEADER */}   
+<table>
+  <tr>
+    <th>Title</th>
+    <th>Github Repo</th>
+    <th>Deployed Link</th>
+  </tr>
+  {/* END OF TABLE HEADER */}
+
+  <tr>
+    <td>JRH Electronics</td>
+    <td>N/A</td>
+    <td><a href="https://jrhelec.com/">JRH Electronics</a></td>
+  </tr>
+
+  <tr>
+    <td>My First Project</td>
+    <td><a href="https://github.com/upennbootcamp23/egerev_css-demo-site">Github Repository</a></td>
+    <td><a href="https://upennbootcamp23.github.io/egerev_css-demo-site/">Deployed Link</a></td>
+  </tr>
+
+  <tr>
+    <td>HoriSEOn</td>
+    <td><a href="https://github.com/upennbootcamp23/seo-website-refactor">Github Repository</a></td>
+    <td><a href="https://upennbootcamp23.github.io/seo-website-refactor/">Deployed Link</a></td>
+  </tr>
+
+  <tr>
+    <td>My Portfolio</td>
+    <td><a href="https://github.com/upennbootcamp23/egerev_portfolio-website">Github Repository</a></td>
+    <td><a href="https://upennbootcamp23.github.io/egerev_portfolio-website/">Deployed Link</a></td>
+  </tr>
+
+  <tr>
+    <td>Password Generator</td>
+    <td><a href="https://github.com/upennbootcamp23/egerev_password-website-refactor">Github Repository</a></td>
+    <td><a href="https://upennbootcamp23.github.io/egerev_password-website-refactor/">Deployed Link</a></td>
+  </tr>
+
+  <tr>
+    <td>JavaScript Quiz</td>
+    <td><a href="https://github.com/upennbootcamp23/egerev_javascript-quiz-website">Github Repository</a></td>
+    <td><a href="https://upennbootcamp23.github.io/egerev_javascript-quiz-website/">Deployed Link</a></td>
+  </tr>
+
+  <tr>
+    <td>Workday Planner</td>
+    <td><a href="https://github.com/upennbootcamp23/egerev_planner-website-refactor">Github Repository</a></td>
+    <td><a href="https://upennbootcamp23.github.io/egerev_planner-website-refactor/">Deployed Link</a></td>
+  </tr>
+
+
+  <tr>
+    <td>Weather Dashboard</td>
+    <td><a href="https://github.com/upennbootcamp23/egerev_weather-website">Github Repository</a></td>
+    <td><a href="https://upennbootcamp23.github.io/egerev_weather-website/">Deployed Link</a></td>
+  </tr>
+
+  <tr>
+    <td>LeafLit</td>
+    <td><a href="https://aaron-heath.github.io/leaflit/">Github Repository</a></td>
+    <td><a href="https://github.com/Aaron-Heath/leaflit">Deployed Link</a></td>
+  </tr>
+
+  <tr>
+    <td>Readme.MD Generator</td>
+    <td><a href="https://github.com/upennbootcamp23/egerev_README-generator">Github Repository</a></td>
+    <td><a href="N/A"></a></td>
+  </tr>
+
+  <tr>
+    <td>SVG Generator</td>
+    <td><a href="https://github.com/upennbootcamp23/egerev_SVG-generator">Github Repository</a></td>
+    <td><a href="N/A">Deployed Link</a></td>
+  </tr>
+
+  <tr>
+    <td>The Online Note Taker</td>
+    <td><a href="https://github.com/upennbootcamp23/egerev_note-taker-refactored">Github Repository</a></td>
+    <td><a href="https://egerev-note-taker-refactored-e24096d10342.herokuapp.com/">Deployed Link</a></td>
+  </tr>
+
+  <tr>
+    <td>Job Generator</td>
+    <td><a href="https://github.com/upennbootcamp23/egerev_MySQL-job-generator">Github Repository</a></td>
+    <td><a href="N/A">Deployed Link</a></td>
+  </tr>
+
+  <tr>
+    <td>E-Commerce, One Piece Style</td>
+    <td><a href="https://github.com/upennbootcamp23/egerev_MySQL-job-generator">Github Repository</a></td>
+    <td><a href="N/A">Deployed Link</a></td>
+  </tr>
+
+  <tr>
+    <td>SVG Generator</td>
+    <td><a href="https://github.com/upennbootcamp23/egerev_SVG-generator">Github Repository</a></td>
+    <td><a href="N/A">Deployed Link</a></td>
+  </tr>
+
+  <tr>
+    <td>The Online Note Taker</td>
+    <td><a href="https://github.com/upennbootcamp23/egerev_note-taker-refactored">Github Repository</a></td>
+    <td><a href="https://egerev-note-taker-refactored-e24096d10342.herokuapp.com/">Deployed Link</a></td>
+  </tr>
+
+  <tr>
+    <td>One Piece Blog</td>
+    <td><a href="https://github.com/upennbootcamp23/egerev_CMS-blog-site">Github Repository</a></td>
+    <td><a href="https://egerev-cmsblogsite-1c7e5ee8496d.herokuapp.com/">Deployed Link</a></td>
+  </tr>
+
+  <tr>
+    <td>A Garden In Paradise</td>
+    <td><a href="https://github.com/jeffch19/garden-bliss">Github Repository</a></td>
+    <td><a href="https://garden-planner19-05823e4485e2.herokuapp.com/">Deployed Link</a></td>
+  </tr>
+
+  <tr>
+    <td>RegEX</td>
+    <td><a href="https://gist.github.com/upennbootcamp23">Github Repository</a></td>
+    <td><a href="N/A">Deployed Link</a></td>
+  </tr>
+
+  <tr>
+    <td>Just A Text Editor, Nothing Personal.</td>
+    <td><a href="https://github.com/upennbootcamp23/egerev_jate-refactor">Github Repository</a></td>
+    <td><a href="https://egerev-just-another-text-editor.onrender.com/">Deployed Link</a></td>
+  </tr>
+
+
+
+</table>
+
+        
+        
         </>
     )
 };
